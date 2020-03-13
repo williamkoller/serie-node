d62040e6f8deeb8d5ed06afd30ec3591131fc37b
William Koller <wkoller25@gmail.com>
2020-03-12 23:58:30 -0300

    call authController

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const app = express();
 const port = 3000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(morgan('dev'));
 
-app.listen(port);
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require("./controllers/authController")(app);
+
+app.listen(port, () => {
+  console.log(Date(), `server running ${port}`);
+  console.log("¯\\_(ツ)_/¯");
+});
