5448efdefdde868f44f7d94b4acbd0adf21621dc
William Koller <wkoller25@gmail.com>
2020-03-14 23:31:58 -0300

    :rocket: validate auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth");
 
 const User = require("../model/User");
 
 const router = express.Router();
+
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    // 86400 expire 1 day
+    expiresIn: 86400
+  });
+}
 
 router.post("/register", async (req, res) => {
   const { email } = req.body;
@@ -14,10 +24,30 @@ router.post("/register", async (req, res) => {
 
     user.password = undefined;
 
-    return res.send({ user });
+    return res.send({
+      user,
+      token: generateToken({ id: user.id })
+    });
   } catch (err) {
-    return res.status(400).send({ err: "Registration failed" });
+    return res.status(400).send({ error: "Registration failed" });
   }
+});
+
+router.post("/authenticate", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) return res.status(400).send({ error: "User not found" });
+
+  if (!(await bcrypt.compare(password, user.password)))
+    return res.status(400).send({ error: "Invalid password" });
+
+  user.password = undefined;
+
+  res.send({
+    user,
+    token: generateToken({ id: user.id })
+  });
 });
 
 module.exports = app => app.use("/auth", router);
