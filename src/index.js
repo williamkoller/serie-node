9fe49c4a3669da157966da47f2a47e5166a61023
William Koller <wkoller25@gmail.com>
2020-03-14 23:33:18 -0300

    :start: adding project controller

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require("./controllers/authController")(app);
+require("./controllers/projectController")(app);
 
 app.listen(port, () => {
   console.log(Date(), `server running ${port}`);
